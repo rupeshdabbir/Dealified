b544c07493b73ddf72c8e454dd4a0f062a3c34a6
Rupesh Dabbir <rdabbir@cisco.com>
2016-11-20 01:54:03 -0800

    Redis fix

--- Dealified/routes/users.js
+++ Dealified/routes/users.js
@@ -8,20 +8,40 @@ mongo.connect(function(_db){
 });
 
 var finalResult={};
-var newData=[];
 
 
-/* GET users listing. */
+ /* POST the Alert data from the UI
+  * There are two conditions:
+  *
+  *  Condition A: if the data is not present, we insert into the redis.
+  *  Condition B: if data is present, we append to the existing record.
+  *
+  */
+
 router.post('/searchData', function(req, res) {
 
   var data = req.body;
+  var newData=[];
   var product = data.tags[0].toLowerCase();
 
-  finalResult[product] = newData;
-  newData.push(data);
+  // finalResult[product] = newData;
+  // newData.push(data);
 
-  //Insert into Redis
-  client.setex(product, 6000000, JSON.stringify(finalResult));
+  client.get(product, function(err, result){
+
+    if(result){
+      console.log("result present"+result);
+      finalResult[product].push(data);
+    }
+    else {
+      //Insert into Redis
+      console.log("Empty ruleset, inserting into DB");
+      finalResult[product] = newData;
+      newData.push(data);
+      client.setex(product, 6000000, JSON.stringify(finalResult));
+    }
+
+  });
 
   res.sendStatus(200);
 });
